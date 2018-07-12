4e302cf52e386c3e48949a38f943b13b5b4063a9
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-12 13:34:02 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,8 @@
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+
 
 # require libraries/modules here
 require 'nokogiri'
@@ -15,5 +17,3 @@ def create_project_hash
 end
 
 create_project_hash
-
-
